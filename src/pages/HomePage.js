15ba90a56563b2fb19c86b6cfdcd3f1b6c97bec3
bhan <bhan@idevel.kr>
2025-03-13 23:53:09 -0700

    fixed typo

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { fetchMovies } from "../api/tmdbApi";
+import { fetchMovies } from "../api/tmdbApi.js";
 
 function HomePage() {
   const [loading, setLoading] = useState(true);
