22e9ccd23d5501f60beb9f5fdc084608442ee1a2
seon022 <seon022@icloud.com>
2025-03-22 01:47:50 +0900

    Feat: Add Navigation bar  - The component is named Navigation for future extensibility

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import MovieIcon from "@mui/icons-material/Movie";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import SearchIcon from "@mui/icons-material/Search";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
+
+const SMALL_SIZE = 20;
+const LARGE_SIZE = 28;
+
+const navItems = [
+	{ label: "Movie", icon: <MovieIcon />, path: "/" },
+	{ label: "Search", icon: <SearchIcon />, path: "/search" },
+	{ label: "Favorite", icon: <FavoriteIcon />, path: "/favorites" },
+	{ label: "Review", icon: <RateReviewIcon />, path: "/review" },
+	{ label: "My Page", icon: <AccountBoxIcon />, path: "/mypage" },
+];
+
+const Navigation = () => {
+	const [value, setValue] = useState(0);
+	const navigate = useNavigate();
+
+	const handleNavigation = (newValue) => {
+		setValue(newValue);
+		navigate(navItems[newValue].path);
+	};
+
+	return (
+		<Paper
+			sx={{
+				position: "fixed",
+				bottom: 0,
+				left: "50%",
+				transform: "translateX(-50%)",
+				width: "100%",
+				maxWidth: "50%",
+				zIndex: 1000,
+				"@media (max-width: 1280px)": {
+					maxWidth: "100%",
+				},
+			}}
+		>
+			<BottomNavigation
+				showLabels
+				value={value}
+				onChange={(event, newValue) => {
+					handleNavigation(newValue);
+				}}
+				sx={{
+					backgroundColor: "primary.light",
+					height: { xs: 60, sm: 70 },
+					"& .MuiBottomNavigationAction-root": {
+						minWidth: 0,
+						maxWidth: "none",
+					},
+					"& .MuiBottomNavigationAction-label": {
+						fontSize: { xs: "10px", sm: "12px", md: "13px" },
+					},
+					"& .Mui-selected": {
+						color: "#fff",
+						"& .MuiSvgIcon-root": {
+							color: "#fff",
+						},
+					},
+				}}
+			>
+				{navItems.map((item, index) => (
+					<BottomNavigationAction
+						key={index}
+						label={item.label}
+						icon={React.cloneElement(item.icon, {
+							sx: {
+								fontSize: { sm: SMALL_SIZE, xl: LARGE_SIZE },
+							},
+						})}
+					/>
+				))}
+			</BottomNavigation>
+		</Paper>
+	);
+};
+export default Navigation;
