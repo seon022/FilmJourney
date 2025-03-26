47390ed181c88edb0db09d7324c1fe49d075b844
bhan <bhan@idevel.kr>
2025-03-26 00:59:26 -0700

    signin with google

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,32 +1,26 @@
 import { useState } from "react";
+import { signInWithGooglePopup } from "../firebase";
 
 function Auth() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response.user.displayName);
+  };
+
   return (
     <div>
       <div className="auth-page"></div>
       <div className="auth-form">
-        <h1>Login</h1>
-        <form>
-          <form>
-            <input
-              name="email"
-              type="text"
-              placeholder="Email"
-              required
-              value={email}
-            />
-            <input
-              name="password"
-              type="password"
-              placeholder="Password"
-              required
-              value={password}
-            />
-            <input type="submit" value="Create Account" />
-          </form>
-        </form>
+        {response.user ? (
+          <h1>Welcome {response.user.displayName}</h1>
+        ) : (
+          <div>
+            <h1>Login</h1>
+            <div>
+              <button onClick={logGoogleUser}>Sign In With Google</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
