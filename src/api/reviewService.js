f9236021f6edd87848cf7ae2bd9840712bd8c6c0
Byol Han <138435498+byol-han@users.noreply.github.com>
2025-04-20 21:37:29 -0700

    [Feature] reviews on movie detail page (#21)
    * updated UI for movie detail page
    
    * review list on detail in progress
    
    * fetch reviews on movie detail page
    
    * fix App.jsx  import line
    
    ---------
    
    Co-authored-by: bhan <bhan@idevel.kr>
    Co-authored-by: seon022 <seon022@icloud.com>

--- src/api/reviewService.js
+++ src/api/reviewService.js
@@ -1,100 +1,122 @@
 import {
-	collection,
-	addDoc,
-	serverTimestamp,
-	getDocs,
-	doc,
-	deleteDoc,
-	updateDoc,
+  collection,
+  addDoc,
+  serverTimestamp,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+  getFirestore,
+  collectionGroup,
+  query,
+  where,
 } from "firebase/firestore";
 
 import useUserStore from "@store/userStore";
 
 import { db } from "../firebase";
+import { getAuth } from "firebase/auth";
 
-const getUserId = () => {
-	const { user } = useUserStore.getState();
-	return user ? user.userId : null;
-};
+const { user } = useUserStore.getState();
+const userId = user ? user.userId : null;
 
 export const addReview = async (movieData, rating, reviewText, watchedDate) => {
-	const userId = getUserId();
-	if (!userId) throw new Error("User is not logged in.");
+  if (!userId) throw new Error("User is not logged in.");
 
-	try {
-		const reviewRef = collection(db, "users", userId, "reviews");
-		await addDoc(reviewRef, {
-			movieId: movieData.id,
-			movieTitle: movieData.title,
-			moviePoster: movieData.posterPath || "",
-			reviewDate: new Date().toISOString().split("T")[0],
-			watchedDate: watchedDate || null,
-			rating,
-			reviewText,
-			createdAt: serverTimestamp(),
-		});
-		console.log(
-			"Successfully saved!",
-			movieData,
-			rating,
-			reviewText,
-			watchedDate
-		);
-	} catch (error) {
-		console.error("Error in review save:", error);
-	}
+  try {
+    const reviewRef = collection(db, "users", userId, "reviews");
+    await addDoc(reviewRef, {
+      movieId: movieData.id,
+      movieTitle: movieData.title,
+      moviePoster: movieData.poster_path || "",
+      reviewDate: new Date().toISOString().split("T")[0],
+      watchedDate: watchedDate || null,
+      rating,
+      reviewText,
+      createdAt: serverTimestamp(),
+    });
+    console.log(
+      "Successfully saved!",
+      movieData,
+      rating,
+      reviewText,
+      watchedDate
+    );
+  } catch (error) {
+    console.error("Error in review save:", error);
+  }
 };
 
 export const updateReview = async (
-	reviewId,
-	movieData,
-	rating,
-	reviewText,
-	watchedDate
+  reviewId,
+  movieData,
+  rating,
+  reviewText,
+  watchedDate
 ) => {
-	const userId = getUserId();
-	if (!userId) throw new Error("User is not logged in.");
+  if (!userId) throw new Error("User is not logged in.");
 
-	try {
-		const reviewDocRef = doc(db, "users", userId, "reviews", reviewId);
-		await updateDoc(reviewDocRef, {
-			movieId: movieData.id,
-			movieTitle: movieData.title,
-			moviePoster: movieData.posterPath || "",
-			watchedDate: watchedDate || null,
-			rating,
-			reviewText,
-			updatedAt: serverTimestamp(),
-		});
-		console.log("Review updated successfully!");
-	} catch (error) {
-		console.error("Error updating review:", error);
-	}
+  try {
+    const reviewDocRef = doc(db, "users", userId, "reviews", reviewId);
+    await updateDoc(reviewDocRef, {
+      movieId: movieData.id,
+      movieTitle: movieData.title,
+      moviePoster: movieData.poster_path || "",
+      watchedDate: watchedDate || null,
+      rating,
+      reviewText,
+      updatedAt: serverTimestamp(),
+    });
+    console.log("Review updated successfully!");
+  } catch (error) {
+    console.error("Error updating review:", error);
+  }
 };
 
 export const getReviews = async () => {
-	const userId = getUserId();
-	try {
-		const reviewRef = collection(db, "users", userId, "reviews");
-		const snapshot = await getDocs(reviewRef);
-		const reviews = snapshot.docs.map((doc) => ({
-			id: doc.id,
-			...doc.data(),
-		}));
-		return reviews;
-	} catch (error) {
-		console.error("Error fetching reviews:", error);
-		return [];
-	}
+  try {
+    const reviewRef = collection(db, "users", userId, "reviews");
+    const snapshot = await getDocs(reviewRef);
+    const reviews = snapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return reviews;
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
+    return [];
+  }
 };
 
 export const deleteReviewFromFirebase = async (reviewId) => {
-	const userId = getUserId();
-	try {
-		const reviewDoc = doc(db, "users", userId, "reviews", reviewId);
-		await deleteDoc(reviewDoc);
-		console.log("Review deleted successfully");
-	} catch (error) {
-		console.error("Error deleting review:", error);
-	}
+  try {
+    const reviewDoc = doc(db, "users", userId, "reviews", reviewId);
+    await deleteDoc(reviewDoc);
+    console.log("Review deleted successfully");
+  } catch (error) {
+    console.error("Error deleting review:", error);
+  }
+};
+
+export const getAllReviewsForMovie = async (movieId) => {
+  try {
+    const auth = getAuth();
+
+    const reviewsQuery = query(
+      collectionGroup(db, "reviews"),
+      where("movieId", "==", Number(movieId))
+    );
+
+    const querySnapshot = await getDocs(reviewsQuery);
+    const reviews = [];
+
+    querySnapshot.forEach((doc) => {
+      reviews.push({ id: doc.id, ...doc.data() });
+    });
+
+    return reviews;
+  } catch (error) {
+    console.error("Error fetching all reviews for movie:", error);
+    return [];
+  }
 };
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,132 +1,186 @@
 import { useEffect, useState } from "react";
-
-import AddIcon from "@mui/icons-material/Add";
-import StarRateIcon from "@mui/icons-material/StarRate";
-import Fab from "@mui/material/Fab";
 import { useParams, useNavigate } from "react-router-dom";
-
+import useUserStore from "../store/userStore";
+// API, components, and store
 import {
-	fetchMovieDetail,
-	fetchCast,
-	fetchSimilarMovies,
+  fetchMovieDetail,
+  fetchCast,
+  fetchSimilarMovies,
 } from "../api/tmdbApi.js";
 import MovieCard from "../components/MovieCard";
 import useMovieStore from "../store/movieStore";
+import { getAllReviewsForMovie } from "../api/reviewService.js";
+// MUI
+import AddIcon from "@mui/icons-material/Add";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import Fab from "@mui/material/Fab";
+import Avatar from "@mui/material/Avatar";
+import Divider from "@mui/material/Divider";
 
 function MovieDetail() {
-	const [loading, setLoading] = useState(true);
-	const [detail, setDetail] = useState([]);
-	const [cast, setCast] = useState([]);
-	const [similar, setSimilar] = useState([]);
-	const { id } = useParams();
-	const navigate = useNavigate();
-	const setMovie = useMovieStore((state) => state.setMovie);
-	const setEditReview = useMovieStore((state) => state.setEditReview);
+  const [loading, setLoading] = useState(true);
+  const [detail, setDetail] = useState([]);
+  const [cast, setCast] = useState([]);
+  const [similar, setSimilar] = useState([]);
+  const [reviews, setReviews] = useState([]);
+  const { id } = useParams();
+  const { user } = useUserStore();
+  const navigate = useNavigate();
+  const setMovie = useMovieStore((state) => state.setMovie);
+  const setEditReview = useMovieStore((state) => state.setEditReview);
 
-	const handleGoToReviewForm = () => {
-		setMovie(detail);
+  const handleGoToReviewForm = () => {
+    setMovie(detail);
 
-		const existingReview = useMovieStore
-			.getState()
-			.reviews.find((review) => review.movieId === detail.id);
+    const existingReview = useMovieStore
+      .getState()
+      .reviews.find((review) => review.movieId === detail.id);
 
-		if (existingReview) {
-			setEditReview(existingReview);
-		} else {
-			setEditReview(null);
-		}
+    if (existingReview) {
+      setEditReview(existingReview);
+    } else {
+      setEditReview(null);
+    }
 
-		navigate(`/review/${id}`);
-	};
-	const getDetail = async () => {
-		setLoading(true);
-		fetchMovieDetail(id);
-		setDetail(await fetchMovieDetail(id));
-		fetchCast(id);
-		setCast(await fetchCast(id));
-		fetchSimilarMovies(id);
-		setSimilar(await fetchSimilarMovies(id));
-		setLoading(false);
-	};
-	useEffect(() => {
-		getDetail();
-	}, []);
-	return (
-		<div className="cont-wrapper">
-			{loading ? (
-				<h1>Loading...</h1>
-			) : (
-				<div className="detail-container">
-					<img
-						src={`https://image.tmdb.org/t/p/w400${detail.poster_path}`}
-						alt={detail.title}
-						className="movie-img"
-					/>
-					<div className="detail-card movie-info">
-						<h1 className="title">{detail.title}</h1>
-						<div>
-							<StarRateIcon fontSize="small" color="rating"></StarRateIcon>
-							<p> {detail.vote_average.toFixed(1)}</p>
-						</div>
-						<ul>
-							{detail.genres.map((g) => (
-								<li key={g.id}>{g.name}</li>
-							))}
-						</ul>
-						<p>year : {detail.release_date.slice(0, 4)}</p>
-						<p>language : {detail.original_language.toUpperCase()}</p>
-						<p>overview : {detail.overview}</p>
-						<ul>
-							<li>Cast :</li>
-							{cast.cast.slice(0, 5).map((c) => (
-								<li key={c.id}>{c.name}</li>
-							))}
-						</ul>
-						<ul>
-							<li>Director :</li>
-							{cast.crew
-								.filter((c) => c.job === "Director")
-								.map((c) => (
-									<li key={c.id}>{c.name}</li>
-								))}
-						</ul>
-					</div>
-					<div className="detail-card">
-						<h3>Reviews</h3>
-					</div>
-					<div className="detail-card">
-						<h3>Similar Movies</h3>
-						<section className="movie-set">
-							<div className="set-list">
-								{similar.map((movie) => (
-									<MovieCard
-										key={movie.id}
-										id={movie.id}
-										poster={movie.poster_path}
-										title={movie.title}
-										aveRate={movie.vote_average.toFixed(1)}
-									/>
-								))}
-							</div>
-						</section>
-					</div>
-					<Fab
-						color="primary"
-						aria-label="add"
-						sx={{
-							position: "fixed",
-							bottom: 90,
-							right: "max(calc((100vw - 760px) / 2 + 20px), 20px)",
-							zIndex: 1000,
-						}}
-						onClick={handleGoToReviewForm}
-					>
-						<AddIcon />
-					</Fab>
-				</div>
-			)}
-		</div>
-	);
+    navigate(`/review/${id}`);
+  };
+  const getDetail = async () => {
+    setLoading(true);
+    fetchMovieDetail(id);
+    setDetail(await fetchMovieDetail(id));
+    fetchCast(id);
+    setCast(await fetchCast(id));
+    fetchSimilarMovies(id);
+    setSimilar(await fetchSimilarMovies(id));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  useEffect(() => {
+    console.log("user", user);
+    if (!user) return; // 아직 로그인 정보 없음
+
+    const fetchReviews = async () => {
+      const fetchedReviews = await getAllReviewsForMovie(id);
+      setReviews(fetchedReviews); // 이제 진짜 배열이 들어감!
+    };
+
+    fetchReviews();
+    console.log("reviews", reviews);
+  }, [user, id]);
+
+  return (
+    <div className="cont-wrapper">
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="detail-container">
+          <img
+            src={`https://image.tmdb.org/t/p/w400${detail.poster_path}`}
+            alt={detail.title}
+            className="movie-img"
+          />
+          <div className="detail-card movie-info">
+            <h1 className="title">{detail.title}</h1>
+            <div className="rating">
+              <StarRateIcon fontSize="small" color="rating"></StarRateIcon>
+              <p> {detail.vote_average.toFixed(1)}</p>
+            </div>
+            <ul className="genre-list">
+              {detail.genres.map((g) => (
+                <li key={g.id}>{g.name}</li>
+              ))}
+            </ul>
+            <div className="info-list">
+              <div>
+                <p className="cate">year</p>
+                <p>{detail.release_date.slice(0, 4)}</p>
+              </div>
+              <div>
+                <p className="cate">language</p>
+                <p>{detail.original_language.toUpperCase()}</p>
+              </div>
+            </div>
+            <div>
+              <p className="sec-title">overview</p>
+              <p>{detail.overview}</p>
+            </div>
+            <div>
+              <p className="sec-title">Cast</p>
+              <p>
+                {cast.cast
+                  .slice(0, 5)
+                  .map((c) => c.name)
+                  .join(", ")}
+              </p>
+            </div>
+            <div>
+              <p className="sec-title">Director</p>
+              <ul>
+                {cast.crew
+                  .filter((c) => c.job === "Director")
+                  .map((c) => (
+                    <li key={c.id}>{c.name}</li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+          <div className="detail-card">
+            <h3>Rivews</h3>
+            <ul className="review-list">
+              {reviews.map((review, index) => (
+                <li key={index} className="review-set">
+                  <Avatar alt={review.name} src={review.avatarUrl} />
+                  <div className="review-content">
+                    <div className="review-header">
+                      <p className="name">{review.userName}</p>
+                      <div className="rating">
+                        <p>{review.rating}</p>
+                        <StarRateIcon fontSize="small" color="rating" />
+                      </div>
+                    </div>
+                    <p className="cont">{review.reviewText}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="detail-card">
+            <h3>Similar Movies</h3>
+            <section className="movie-set">
+              <div className="set-list">
+                {similar.map((movie) => (
+                  <MovieCard
+                    key={movie.id}
+                    id={movie.id}
+                    poster={movie.poster_path}
+                    title={movie.title}
+                    aveRate={movie.vote_average.toFixed(1)}
+                  />
+                ))}
+              </div>
+            </section>
+          </div>
+          <Fab
+            color="primary"
+            aria-label="add"
+            sx={{
+              position: "fixed",
+              bottom: 90,
+              right: "max(calc((100vw - 760px) / 2 + 20px), 20px)",
+              zIndex: 1000,
+            }}
+            onClick={handleGoToReviewForm}
+          >
+            <AddIcon />
+          </Fab>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default MovieDetail;
