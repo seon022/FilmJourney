c659d9335bf6758728db02fee5e5c2e6c32e8762
byol-han <byolhandev@gmail.com>
2025-05-26 09:29:35 -0700

    create edit profile page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ReviewPage from "@pages/ReviewPage";
 
 import Navigation from "./components/Navigation";
 import Auth from "./pages/Auth";
+import EditProfile from "./pages/EditProfile";
 import FavoritesPage from "./pages/FavoritesPage";
 import Home from "./pages/Home";
 import MovieDetail from "./pages/MovieDetail";
@@ -12,21 +13,22 @@ import ReviewForm from "./pages/ReviewForm";
 import SearchPage from "./pages/SearchPage";
 
 function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/auth" element={<Auth />} />
-				<Route path="/" element={<Home />} />
-				<Route path="/movie/:id" element={<MovieDetail />} />
-				<Route path="/review/:id" element={<ReviewForm />} />
-				<Route path="/search" element={<SearchPage />} />
-				<Route path="/favorites" element={<FavoritesPage />} />
-				<Route path="/mypage" element={<MyPage />} />
-				<Route path="/review" element={<ReviewPage />} />
-			</Routes>
-			<Navigation />
-		</Router>
-	);
+  return (
+    <Router>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/review/:id" element={<ReviewForm />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/review" element={<ReviewPage />} />
+      </Routes>
+      <Navigation />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/EditProfile.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+
+import { Container, Typography, Box, Button } from "@mui/material";
+
+import BackHeader from "../components/BackHeader";
+
+const EditProfile = () => {
+  useEffect(() => {});
+
+  return (
+    <div>
+      <BackHeader text="Edit Profile" />
+      <Container></Container>
+    </div>
+  );
+};
+
+export default EditProfile;
