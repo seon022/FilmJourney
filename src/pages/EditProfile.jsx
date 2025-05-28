6d03c584f91590a027333dd2fc4a25db06ec84f3
byol-han <byolhandev@gmail.com>
2025-05-28 09:51:51 -0700

    update UI for Edit Profile page

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,16 +1,45 @@
-import React, { useEffect } from "react";
-
-import { Container, Typography, Box, Button } from "@mui/material";
+import { useEffect } from "react";
+import { Container, Box, TextField, Typography, Button } from "@mui/material";
 
 import BackHeader from "../components/BackHeader";
+import useUserStore from "../store/userStore";
 
 const EditProfile = () => {
+  const { user } = useUserStore();
   useEffect(() => {});
 
   return (
     <div>
       <BackHeader text="Edit Profile" />
-      <Container></Container>
+      <Container
+        maxWidth="sm"
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography variant="p" mb={1}>
+          USERNAME
+        </Typography>
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <TextField
+            id="updateUsername"
+            value={user.displayName.split(" ")[0]}
+          />
+        </Box>
+        <Box
+          component="form"
+          sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+          noValidate
+          autoComplete="off"
+        >
+          <Button variant="contained" sx={{ minWidth: 150 }}>
+            Update
+          </Button>
+        </Box>
+      </Container>
     </div>
   );
 };
