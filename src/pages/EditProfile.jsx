9b5f86f36016ca034efc03c0f5a81a05b51ee6f0
seon022 <seon022@icloud.com>
2025-06-02 01:22:37 +0900

    fix: After updating displayname,Redirecting to mypage

--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -1,77 +1,80 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { Container, Box, TextField, Typography, Button } from '@mui/material';
-import { updateProfile } from 'firebase/auth';
+import { Container, Box, TextField, Typography, Button } from "@mui/material";
+import { updateProfile } from "firebase/auth";
 
-import BackHeader from '../components/BackHeader';
-import { auth } from '../firebase';
-import useUserStore from '../store/userStore';
+import BackHeader from "../components/BackHeader";
+import { auth } from "../firebase";
+import useUserStore from "../store/userStore";
+import { useNavigate } from "react-router-dom";
 
 const EditProfile = () => {
-  const { user } = useUserStore();
-  const [username, setUsername] = useState('');
+	const { user } = useUserStore();
+	const [username, setUsername] = useState("");
+	const navigate = useNavigate();
 
-  useEffect(() => {
-    if (user?.displayName) {
-      setUsername(user.displayName.split(' ')[0]);
-    }
-  }, [user]);
+	useEffect(() => {
+		if (user?.displayName) {
+			setUsername(user.displayName.split(" ")[0]);
+		}
+	}, [user]);
 
-  const handleChange = (e) => {
-    setUsername(e.target.value);
-  };
+	const handleChange = (e) => {
+		setUsername(e.target.value);
+	};
 
-  const handleUpdateUsername = async () => {
-    try {
-      await updateProfile(auth.currentUser, {
-        displayName: username,
-      });
-      alert('Successfully updated your name!');
-    } catch (error) {
-      console.error('failed to update username:', error);
-      alert('failed to update username');
-    }
-  };
+	const handleUpdateUsername = async () => {
+		try {
+			await updateProfile(auth.currentUser, {
+				displayName: username,
+			});
+			alert("Successfully updated your name!");
+			navigate("/mypage");
+		} catch (error) {
+			console.error("failed to update username:", error);
+			alert("failed to update username");
+		}
+	};
 
-  return (
-    <div>
-      <BackHeader text='Edit Profile' />
-      <Container
-        maxWidth='sm'
-        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
-      >
-        <Typography variant='p' mb={1}>
-          USERNAME
-        </Typography>
-        <Box
-          component='form'
-          sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-          noValidate
-          autoComplete='off'
-        >
-          <TextField
-            id='updateUsername'
-            value={username}
-            onChange={handleChange}
-          />
-        </Box>
-        <Box
-          component='form'
-          sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
-          noValidate
-          autoComplete='off'
-        >
-          <Button
-            variant='contained'
-            sx={{ minWidth: 150 }}
-            onClick={handleUpdateUsername}
-          >
-            Update
-          </Button>
-        </Box>
-      </Container>
-    </div>
-  );
+	return (
+		<div>
+			<BackHeader text="Edit Profile" />
+			<Container
+				maxWidth="sm"
+				sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+			>
+				<Typography variant="p" mb={1}>
+					USERNAME
+				</Typography>
+				<Box
+					component="form"
+					sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+					noValidate
+					autoComplete="off"
+				>
+					<TextField
+						id="updateUsername"
+						value={username}
+						onChange={handleChange}
+					/>
+				</Box>
+				<Box
+					component="form"
+					sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+					noValidate
+					autoComplete="off"
+				>
+					<Button
+						variant="contained"
+						sx={{ minWidth: 150 }}
+						onClick={handleUpdateUsername}
+					>
+						Update
+					</Button>
+				</Box>
+			</Container>
+		</div>
+	);
 };
 
 export default EditProfile;
