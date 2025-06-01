4b85c9e0dffe3bfb5b8411f2ccff715350474558
Byol Han <138435498+byol-han@users.noreply.github.com>
2025-06-01 09:14:47 -0700

    Feature/edit profile (#26)
    * create edit profile page
    
    * update UI for Edit Profile page
    
    * edit usermane

---
+++ src/pages/EditProfile.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from 'react';
+
+import { Container, Box, TextField, Typography, Button } from '@mui/material';
+import { updateProfile } from 'firebase/auth';
+
+import BackHeader from '../components/BackHeader';
+import { auth } from '../firebase';
+import useUserStore from '../store/userStore';
+
+const EditProfile = () => {
+  const { user } = useUserStore();
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {
+    if (user?.displayName) {
+      setUsername(user.displayName.split(' ')[0]);
+    }
+  }, [user]);
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleUpdateUsername = async () => {
+    try {
+      await updateProfile(auth.currentUser, {
+        displayName: username,
+      });
+      alert('Successfully updated your name!');
+    } catch (error) {
+      console.error('failed to update username:', error);
+      alert('failed to update username');
+    }
+  };
+
+  return (
+    <div>
+      <BackHeader text='Edit Profile' />
+      <Container
+        maxWidth='sm'
+        sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+      >
+        <Typography variant='p' mb={1}>
+          USERNAME
+        </Typography>
+        <Box
+          component='form'
+          sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+          noValidate
+          autoComplete='off'
+        >
+          <TextField
+            id='updateUsername'
+            value={username}
+            onChange={handleChange}
+          />
+        </Box>
+        <Box
+          component='form'
+          sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+          noValidate
+          autoComplete='off'
+        >
+          <Button
+            variant='contained'
+            sx={{ minWidth: 150 }}
+            onClick={handleUpdateUsername}
+          >
+            Update
+          </Button>
+        </Box>
+      </Container>
+    </div>
+  );
+};
+
+export default EditProfile;
